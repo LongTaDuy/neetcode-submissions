023c8a3b6a2c559d3eac32621848f093fea66612
Long (Louis) Ta <ta_l7@denison.edu>
2026-06-08 21:07:46 -0400

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftmax = 0
+        leftmin = 0
+        for i in s:
+            if i == "(":
+                leftmin, leftmax = leftmin + 1, leftmax + 1
+            elif i == ")":
+                leftmin, leftmax = leftmin - 1, leftmax - 1
+            else:
+                leftmin, leftmax = leftmin - 1, leftmax + 1
+            if leftmin < 0:
+                leftmin = 0
+            if leftmax < 0:
+                return False
+        return leftmin == 0
+        
