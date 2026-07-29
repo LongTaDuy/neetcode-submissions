9345cc5807255db04af12fc2243eb5b22f6a79fe
Long (Louis) Ta <ta_l7@denison.edu>
2026-07-29 16:28:07 +0700

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def backtrack(start, sub):
+            if len(sub) == k:
+                res.append(sub.copy())
+                return
+            for i in range(start, n + 1):
+                sub.append(i)
+                backtrack(i + 1, sub)
+                sub.pop()
+        backtrack(1, [])
+        return res
