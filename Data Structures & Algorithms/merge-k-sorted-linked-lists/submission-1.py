60b2b6c44377e474e109165bbfc1ad82f40c8508
Long (Louis) Ta <ta_l7@denison.edu>
2026-08-18 22:00:10 +0700

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergetwolist(self, l1, l2):
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val > l2.val:
+            head = l2
+            l2 = l2.next
+        else:
+            head = l1
+            l1 = l1.next
+        cur = head
+        while l1 and l2:
+            if l1.val > l2.val:
+                cur.next = l2
+                cur = cur.next
+                l2 = l2.next
+            else:
+                cur.next = l1
+                cur = cur.next
+                l1 = l1.next
+        if l1:
+            cur.next = l1
+        if l2:
+            cur.next = l2
+        return head
+        
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        while len(lists) > 1:
+            mergelist = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                mergelist.append(self.mergetwolist(l1, l2))
+            lists = mergelist
+        return lists[0]
